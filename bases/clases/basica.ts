68ec3dafe53269dbd534d46ee336dcd13dd52bbb
Raul Chauvin <raul.chauvin@telefonica.com>
2023-11-16 13:50:06 -0500

    Forma corta de incializar un objeto en typescript

--- bases/clases/basica.ts
+++ bases/clases/basica.ts
@@ -2,20 +2,32 @@
 
     class Avenger {
         
-        private name: string;
-        private team: string;
-        public realName?: string;
+        // private name: string = 'Scott Lang';
+        // private team: string;
+        // public realName?: string;
         static avgAge: number = 35;
 
-        constructor(name:string, team: string, realName?: string){
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
+        // constructor(name:string, team: string, realName?: string){
+        //     this.name = name;
+        //     this.team = team;
+        //     this.realName = realName;
+        // }
+
+        // Forma corta de inicializar el objeto
+        constructor(
+            private name: string, 
+            private team: string, 
+            avgAge: number,
+            public realName?: string
+        ) {
+            Avenger.avgAge = avgAge;
         }
+
+
 
     }
 
-    const antman: Avenger = new Avenger('Antman', 'Team Capi');
+    const antman: Avenger = new Avenger('Antman', 'Team Capi', 35, 'Scott Lang');
 
     console.log(antman);
     console.log(Avenger.avgAge);
--- bases/myapp.js
+++ bases/myapp.js
@@ -1,14 +1,15 @@
 "use strict";
 (() => {
     class Avenger {
-        constructor(name, team, realName) {
+        constructor(name, team, avgAge, realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
+            Avenger.avgAge = avgAge;
         }
     }
     Avenger.avgAge = 35;
-    const antman = new Avenger('Antman', 'Team Capi');
+    const antman = new Avenger('Antman', 'Team Capi', 35, 'Scott Lang');
     console.log(antman);
     console.log(Avenger.avgAge);
 })();

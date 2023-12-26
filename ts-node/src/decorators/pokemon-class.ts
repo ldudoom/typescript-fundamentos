26656d169a5098321226553f7adb2a1525151a47
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-12-26 16:45:44 -0500

    Ejemplo de un decorador - Bloquear prototipo

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -2,11 +2,28 @@ function printToConsole( constructor: Function ){
     console.log(constructor);
 }
 
-@printToConsole
+const printToConsoleConditional = ( print: boolean = false ): Function => {
+    if( print ){
+        return printToConsole;
+    } else {
+        return () => {};
+    }
+}
+
+
+const bloquearPrototipo = function( constructor: Function ) {
+    Object.seal( constructor );
+    Object.seal( constructor.prototype );
+}
+
+
+@bloquearPrototipo
+@printToConsoleConditional( true )
 export class Pokemon {
 
     public publicApi: string = 'https://pokeapi.co';
     constructor(
         public name: string
     ){}
+
 }
